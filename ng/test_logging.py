e19b9cc2ad68b9d955d8763488029337525250c4
jmward <jmward@penguin>
2025-07-01 17:50:47 -0500

    up

--- ng/test_logging.py
+++ ng/test_logging.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 """
-Test script to verify file logging functionality
+Test script to verify file logging functionality - improved version
 """
 
 import os
-import time
-from logging_setup import get_logger
+import sys
 
-# Set LOG_FILE directly for this test
+# Force reload of config after setting env var
 os.environ["LOG_FILE"] = "test.log"
+
+# Make sure we're using our modified config
+import config
+print(f"LOG_FILE config value: {config.LOG_FILE}")
+
+# Now import the logger
+from logging_setup import get_logger
 
 # Get a logger after setting the env var
 logger = get_logger("test_script")
