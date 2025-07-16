d0f07616acbab428a7417cc9187fc0e6272b2cb3
jmward <jmward@penguin>
2025-07-16 13:09:13 -0500

    up

---
+++ m4/funcs2.py
@@ -0,0 +1,231 @@
+#from utils import *
+import json
+#from . import utils
+
+
+def mkdir(path: str) -> int:
+  """
+  creates a directory at the specified path
+
+  Args:
+   path (str): the name of the file.  can be absolute or relative.
+
+  Returns:
+    int: if it's a 0 it succeeded, if it's anything non-zero it failed.
+  """
+  print("*"*50)
+  print('MKDIR', path)
+  print("*"*50)
+  return 0
+
+
+def read(path: str) -> str:
+  """
+  read the contents of a file located at the specified path
+
+  Args:
+   path (str): the name of the file.  can be absolute or relative.
+
+  Returns:
+    str: the content of the file
+  """
+  print("READ1")
+  ret = ''.join(open(path).readlines())
+  print("READ2", repr(ret))
+  return ret
+
+
+def respond_to_user(message: str) -> str:
+  """
+  Gives a response to a user,
+  in case we can't find any other toolsg to use.
+
+  Args:
+    message (str): What to send to the user
+                   so he reads what we sent.
+
+  Returns:
+    str: The result of telling the user our message.
+  """
+  print("*"*50)
+  print("MSG", type(message), message)
+  print("="*50)
+  #utils.display_to_user(message)
+  utils.get_display_to_user_func()(message)
+  print("*"*50)
+  #mesg = utils.get_user_input_func()(record_response=False)
+  mesg = utils.get_user_input_func()(record_response=False)
+  print("*", mesg)
+  #mesg = utils.get_user_input(record_response=False)
+  return mesg
+
+
+def add_two_numbers(a: int, b: int) -> int:
+  """
+  calculate the sum of two numbers added together
+
+  Args:
+    a (int): The first number
+    b (int): The second number
+
+  Returns:
+    int: The sum of the two numbers added together
+  """
+  # The cast is necessary as returned tool call
+  # arguments don't always conform exactly to schema
+  # E.g. this would prevent "what is 30 + 12"
+  # to produce '3012' instead of 42
+  return int(a) + int(b)
+
+
+
+
+def weather_forecast(location: str) -> str:
+  """
+  This will tell you what the weather's going to be like soon in the specified location.
+
+  Args:
+    location (str): The location of the weather forecast.  Examples would include buffalo, or 78712', here, or outside.
+
+  Returns:
+    str: This is the weather forecast for the location.  if it's blank, that means we couldnt find the location.
+  """
+  print("FIND THE WEATHER FOR", type(location), repr(location))
+  return "Sunny with a 10% chance of rain"
+
+
+
+def create_new_tweet(
+    content: str,
+    when: str
+                     ) -> str:
+  """
+  this schedules/creates a new tweet event for social type of X/Twitter and also opens up a window for the user
+  also known as scheduling a new tweet.
+
+  Args:
+    content (str): The text of the tweet to be posted.  needs to less than 160 characters in length.
+    when (str): When the tweet is scheduled to be posted.  this can be an iso date, or a relative date such as 'today', 'yesterday', 'the day after tomorrow', 'next thursday', 'next next thursday', 'the wednesday past next'.
+
+  Returns:
+    int: the id for the new tweet event.  0 means there was an error.
+  """
+
+  """
+    title (str): the title of the Tweet
+    description (str): The description of the tweet.  needs to be under 80 characters in length.
+  """
+  print("SCHEDULE TWEET", when, content)
+  #print("SCHEDULE TWEET", title, description)
+  return json.dumps(dict(
+      result=dict(
+          id="200",
+#          title=title
+      )))
+      
+
+
+def reschedule_tweet(#title: str, description: str,
+    id: str,
+                     when: str
+                     ) -> str:
+  """
+  this reschedules an existing tweet to a new time/date.
+  it's also knows as changing the time or date on a tweet.
+  it also opens up a window for the user if they are logged in.
+
+  Args:
+    content (str): The text of the tweet to be posted.  needs to less than 160 characters in length.
+    when (str): When the tweet is scheduled to be posted.  this can be an iso date, or a relative date such as 'today', 'yesterday', 'the day after tomorrow', 'next thursday', 'next next thursday', 'the wednesday past next'.
+
+  Returns:
+    int: the id for the new event.  0 means there was an error.
+  """
+
+  """
+    title (str): the title of the Tweet
+    description (str): The description of the tweet.  needs to be under 80 characters in length.
+  """
+  print("RESCHEDULE TWEET", id)
+  #print("SCHEDULE TWEET", title, description)
+  return json.dumps(dict(
+      result=dict(
+          id=id,
+#          title=title
+      )))
+      
+
+
+def delete_tweet(id: str) -> str:
+  """
+  this deletes a tweet event for social type of X/Twitter
+
+  Args:
+    id (str): the id of tweet event
+
+  Returns:
+    int: the id of the deleted tweet event.  0 means there was an error.
+  """
+  print("DELETE TWEET", repr(id))
+  return json.dumps(dict(
+      result=dict(
+          id=id
+      )))
+      
+
+def edit_tweet(id: str) -> int:
+  """
+  this edits a tweet event for social type of X/Twitter.
+  it does this by popping up a modal dialog box for the user
+
+  Args:
+    id (str): the id of tweet event
+
+  Returns:
+    int: the id for the event.  0 means there was an error.
+  """
+  print("EDIT (AS IN POP UP) TWEET", repr(id))
+  return int(id)
+      
+
+def subtract_two_numbers(a: int, b: int) -> int:
+  """
+  Subtract two numbers
+  """
+
+  # The cast is necessary as returned tool call
+  # arguments don't always conform exactly to schema
+  return int(a) - int(b)
+
+
+subtract_two_numbers.tool = {
+  'type': 'function',
+  'function': {
+    'name': 'subtract_two_numbers',
+    'description': 'Subtract two numbers',
+    'parameters': {
+      'type': 'object',
+      'required': ['a', 'b'],
+      'properties': {
+        'a': {'type': 'integer',
+              'description': 'The first number'},
+        'b': {'type': 'integer',
+              'description': 'The second number'},
+      }
+    }
+  }
+}
+
+
+#def xx():
+#    print("XX1")
+#    zzz = utils.get_display_to_user_func()
+#    print("ZZZ", zzz)
+#    print("XX9")
+#    pass
+
+
+Tools = list(getattr(v, 'tool', v)
+             for k,v in globals().items()
+             if (type(v)==type(lambda:1) and
+                 not k.startswith('_')))
---
+++ m4/session.py
@@ -0,0 +1,185 @@
+from typing import Any, Dict, List, Optional, Union, cast
+import os, time, json, websocket, ollama
+import db_sync as db
+from logging_setup import get_logger
+import traceback
+
+
+logger = get_logger(__name__)
+
+
+def recv(ws):
+    raw = ws.recv()
+    if not raw:
+        raise EOFError
+    return json.loads(raw)
+
+def send(ws, msg):
+    return ws.send( json.dumps(msg) )
+
+def mesg(method, **params):
+    return dict(method=method, params=params)
+
+def pub(ws, channel, content='', **kw):
+    return send(ws, mesg('pub',
+                         channel = channel,
+                         content = content, **kw))
+
+
+NAME = os.getenv('NAME','llm')
+IN_CHANNEL  = NAME+'-in'
+OUT_CHANNEL = NAME+'-out'
+WS_BASE = "ws://localhost:5002/ws"
+
+
+class EphemeralSessionProxy:
+
+    def __init__(_, uuid, session_id, funcs,
+                 ws, model, tools, out_channel):
+        _.uuid, _.session_id, _.funcs = uuid, session_id, funcs
+        _.ws, _.model, _.tools      = ws, model, tools
+        _.out_channel = out_channel
+        pass
+
+    def materialize_history(_):
+        """
+        this thin wrapper will grow into its own subststem
+        """
+        ret = []
+        for msg in db.load_simplified_convo(_.session_id):
+            ret.append(msg)
+            pass
+        return ret
+
+    def pub_content(_, content, role, **kw):
+        pub(_.ws, _.out_channel, content, role=role, **kw)
+
+    def append_hist(_, content, role='user', **kw):
+        print("QQQQ KW", kw)
+        muid = db.create_memory(session_id=_.session_id,
+                                user_id=_.uuid,
+                                content=content,
+                                role=role,
+                                kind='history',
+                                **kw)
+        euid = db.create_memory_edge(muid, _.session_id, 'belongs_to')
+        return muid
+
+    def append_user(_, content, role):
+        _id = _.append_hist(content, role=role)
+        return _id
+
+    def append_tool(_, name, arguments, output):
+        tool_calls = [ dict( function = dict( name=name,
+                                              arguments=arguments)) ]
+        role='assistant'
+        print("XXXX", dict(role=role, tool_calls=tool_calls, xx=1))
+        _id = _.append_hist('', role=role, tool_calls=tool_calls, xx=1)
+
+        role='tool'
+        print("YYYY",dict(content=str(output), role=role, tool_name=name, yy=2))
+        _id = _.append_hist(content=str(output), role=role, tool_name=name, yy=2)
+        return _id
+
+    def chat_llm(_) -> ollama.ChatResponse:
+        retries = -1
+        messages = _.materialize_history()
+        for m in messages:
+            print("  ==  ", m)
+            pass
+        while 1:
+            retries += 1
+            if retries > 3:
+                print("TOO MANY RETRIES")
+                raise SystemExit(1)
+            return ollama.chat(model=_.model, messages=messages,
+                               tools=_.tools, stream=True, format='json')
+        pass
+
+    def chat_round(_, content, role='user'):
+        """
+        a round is where each conversationalist takes a turn
+        in LLM world, this is a user comment followed by
+        as assistant comment with optional tool calling in
+        the middle
+        """
+
+        # save the message
+        _.append_user(content, role)
+
+        # process results
+        for msg in _.chat_llm():
+            message, done = msg.message, msg.done
+
+            print(("MSG", done, message))
+            
+            if not message:
+                print("NO MESSAGE")
+                raise exit(1)
+
+            if message.get('content') is None and not message.tool_calls:
+                print("THIS NO CONTENT AND NO TOOL CALLS (maybe an image?)")
+                raise exit(5)
+
+            if message.content is not None and not message.tool_calls:
+                _.append_hist(message.content, message.role, done=done, qq=11)
+                _.pub_content(message.content, message.role, done=done, qq=11)
+                continue
+
+            assert( len(message.tool_calls) == 1 )
+
+            tool_call = message.tool_calls[0].function
+
+            name, arguments, done = \
+                (tool_call.name, tool_call.arguments, done)
+
+            if name == 'respond_to_user':
+                # DON'T call a function here, we'll just handle it right here ourselves
+                role = 'assistant'
+                content = arguments['message']
+                if 1:
+                    _.append_hist(content, role, done=done)
+                    _.pub_content(content, role, done=done)        
+                else:
+                    _.append_tool(name, arguments, output)
+                    _.pub_content(content, role, done=done)
+                    pass 
+                continue
+
+            function_to_call = getattr(_.funcs, name, '')
+
+            if not function_to_call:
+                print('Function', name, 'not found')
+                raise exit(1)
+
+            try:
+                output = function_to_call(**arguments)
+            except:
+                output = traceback.format_exc()
+                print("\\ERROR", '*'*40)
+                print(output)
+                print( "/ERROR", '*'*40)
+                pass
+
+            _.append_tool(name, arguments, output)
+
+            for msg in _.chat_llm():
+
+                message, done = msg.message, msg.done
+
+                if not message:
+                    raise exit(1)
+
+                elif     message.content and not message.tool_calls:
+                    _.append_hist(message.content, message.role, done=done, z10=30)
+                    _.pub_content(message.content, message.role, done=done, yy=3)
+
+                elif not message.content and     message.tool_calls:
+                    raise Exception('too deep')
+
+                else:
+                    if not done:
+                        raise Exception('wtf')
+
+                    #_.append_hist(message.content, role, done=done, z20=45)
+                    #_.pub_content(message.content, role, done=done, yy=2)
