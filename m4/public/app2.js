3f1a35429c940e219ee9d8014fdb00df917c3b8f
jmward <jmward@penguin>
2025-07-20 16:02:22 -0500

    up

--- m4/public/app2.js
+++ m4/public/app2.js
@@ -1,7 +1,20 @@
-const  app = (new class App extends WsApp {
+const app = (new class App extends WsApp {
     _ondata(params){
 	print("DATA", dumps(params))
     }
+    keypress(e){
+	if(e.key!='Enter')return
+	//console.log("KEYPRESS1 "+e.key)
+	const input = e.target.value.trim()
+	e.target.value = ''
+	e.target.blur()
+	if(!input)return
+	//e.target.focus()
+	console.log("INPUT "+input)
+	user(input)
+    }
+    top(){window.scrollTo(0, 0)}
+    bot(){window.scrollTo(0, document.body.scrollHeight)}
 }).connect()
-const  sys = (content, channel)=> app.pub(content, 'system')
-const user = (content, channel)=> app.pub(content)
+const sys = (content, channel)=> app.pub(content, 'system')
+const user= (content, channel)=> app.pub(content)
