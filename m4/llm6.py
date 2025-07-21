4d60fc237ae241a4b84d9b7ce6a0805d45e614bb
jmward <jmward@penguin>
2025-07-21 18:01:47 -0500

    up

--- m4/llm6.py
+++ m4/llm6.py
@@ -6,23 +6,25 @@ import funcs2 as funcs
 from session import EphemeralSessionProxy as ESP, chat_round
 class Convo(SubAgentBase):
     def __init__(_,tools=funcs.Tools, model=os.getenv('MODEL', 'llama3.1')):
-        _.tools, _.model, _.children = tools, model, dict()
+        _.tools, _.model, _.models, _.children = tools, model, dict(), dict()
         return
-    def _kill_if_possible(_, key):
+    def kill_if_possible(_, key):
         if kid:= _.children.pop(key, None):
             gevent.kill(kid)
             print("INTERRUPT THE CURRENT PROCESS", key, kid)
             pass
         return
     def _pub(_, content, uuid, session, model='', toolset='', **kw):
-        def bg_pub(key):
-            sess = ESP(uuid, session, funcs, _.ws(), model or _.model, _.tools)
+        key = ( uuid, session )
+        model = model or _.models.get(key) or _.model
+        _.models[key] = model
+        _.kill_if_possible(key)
+        def bg_pub():
+            sess = ESP(uuid, session, funcs, _.ws(), model, _.tools)
             chat_round(sess, content, OUT_CHANNEL)
             del _.children[key]
             return
-        key = ( uuid, session )
-        _._kill_if_possible(key)
-        _.children[key] = gevent.spawn(bg_pub, key)
+        _.children[key] = gevent.spawn(bg_pub)
         return
     pass
 if __name__ == '__main__':
