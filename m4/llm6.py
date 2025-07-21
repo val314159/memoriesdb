1bd24b7671ed0e8995bfdb7f74a1b934a26308c5
jmward <jmward@penguin>
2025-07-21 17:24:18 -0500

    up

--- m4/llm6.py
+++ m4/llm6.py
@@ -9,16 +9,18 @@ class Convo(SubAgentBase):
         _.tools, _.model = tools, model
         _.child = None
         return
-    def _pub(_, content, uuid, session, model='', toolset='', **kw):
-        def bg_pub():
-            sess = ESP(uuid, session, funcs, _.ws(), model or _.model, _.tools)
-            chat_round(sess, content, OUT_CHANNEL)
-            _.child = None
-            return
+    def _kill_if_possible(_):
         if _.child:
             print("INTERRUPT THE CURRENT PROCESS")
             _.child = gevent.kill(_.child)
             pass
+        return
+    def _pub(_, content, uuid, session, model='', toolset='', **kw):
+        def bg_pub():
+            sess = ESP(uuid, session, funcs, _.ws(), model or _.model, _.tools)
+            _.child = chat_round(sess, content, OUT_CHANNEL)
+            return
+        _.child = _._kill_if_possible()
         _.child = gevent.spawn(bg_pub)
         return
     pass
